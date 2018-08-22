b3721a2e11c2bed12239455b0278d33fd6d7e000
Kayla Cross <kac288@georgetown.edu>
2018-08-22 17:00:48 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -27,7 +27,6 @@ class PigLatinizer
         new_phrase << word[3..-1] + word[0..2] + @@appender
       end
       puts "ORIGINAL WORD: " + word
-      puts "NEW WORD: " + new_phrase.last
     end
 
     new_phrase.join(" ")
