0bd7aa6aaf5a36058ac165c8d0ba4c53d0c0d245
Kayla Cross <kac288@georgetown.edu>
2018-08-22 16:34:34 +0000

    automatically backed up by learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,14 @@
+class PigLatinizer
+
+  attr_accessor :text
+
+  CONSTANT_CONSONANTS = "bl br ch cl cr dr fl fr gl gr pl pr sc sh sk sl sm sn sp st sw th tr tw wh wr sch scr shr sph spl spr squ str thr"
+
+  def initialize(text)
+    @piglatinized_text = text
+  end
+
+  
+
+
+end
