00d900d3067eb649cb52c325cccbb57ae49e081d
Kayla Cross <kac288@georgetown.edu>
2018-08-22 16:37:21 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,9 +3,9 @@ class PigLatinizer
   attr_accessor :text
 
   @@CONSTANT_CONSONANTS = ['bl', 'br', 'ch', 'cl', 'cr', 'dr', 'fl', 'fr', 'gl', 'gr', 'pl', 'pr', 'sc',
-                          'sh', 'sk', 'sl', 'sm', 'sn', 'sp', 'st', 'sw', 'th', 'tr', 'tw', 'wh', 'wr', 
+                          'sh', 'sk', 'sl', 'sm', 'sn', 'sp', 'st', 'sw', 'th', 'tr', 'tw', 'wh', 'wr',
                           'sch', 'scr', 'shr', 'sph', 'spl', 'spr' 'squ', 'str', 'thr']
-  @@CONSTANT_VOWELS = 
+  @@CONSTANT_VOWELS = ['a', 'e', 'i', 'o', 'u']
 
   def initialize(text)
     @piglatinized_text = text
